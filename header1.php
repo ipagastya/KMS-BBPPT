0285a739c215c2b384a8bab48123e8ae9d1a5713
iputuaw <awerdikatama@gmail.com>
2021-05-16 23:46:04 +0700

    test header

--- header1.php
+++ header1.php
@@ -36,7 +36,7 @@
   <!-- navbar-->
     <header class="header" >
       <div role="navigation" class="navbar navbar-default"  style="background-color: #466B97; border-color: #466B97; border-radius: 0px;">
-        <div class="container">
+        <div>
           <div class="navbar-header"><a href="index.php" class="navbar-brand"><img src="src/image/logokms.png" style="width: 200px"></a>
             <div class="navbar-buttons">
               <button type="button" data-toggle="collapse" data-target=".navbar-collapse" class="navbar-toggle navbar-btn">Menu</i></button>
