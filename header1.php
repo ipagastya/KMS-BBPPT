011863682b67bf22c246258394f4248fc46111fc
iputuaw <awerdikatama@gmail.com>
2021-05-16 23:15:14 +0700

    test change logo

--- header1.php
+++ header1.php
@@ -27,10 +27,10 @@
 
     <!-- Javascript files-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <!-- <script src="libs/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="libs/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="libs/bootstrap/dist/js/jquery.cookie.js"> </script>
     <script src="libs/bootstrap/dist/js/lightbox.min.js"></script>
-    <script src="libs/bootstrap/dist/js/front.js"></script> -->
+    <script src="libs/bootstrap/dist/js/front.js"></script>
 </head>
 <body>
   <!-- navbar-->
