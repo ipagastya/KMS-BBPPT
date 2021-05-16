979dc40f4d01208e0e52ac0c73fc8c4b5b80d242
iputuaw <awerdikatama@gmail.com>
2021-05-16 22:37:04 +0700

    change database connection ip

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,18 @@
     <?php 
-        $koneksi = mysqli_connect("http://sql102.epizy.com","epiz_28500943","2kiHEuD0ypoRX4","epiz_28500943_proto_kms_bbppt");
+        // $koneksi = mysqli_connect("http://sql102.epizy.com","epiz_28500943","2kiHEuD0ypoRX4","epiz_28500943_proto_kms_bbppt");
      
-        if (mysqli_connect_errno()){
-            echo "Koneksi database gagal : " . mysqli_connect_error();
-        }
+        // if (mysqli_connect_errno()){
+        //     echo "Koneksi database gagal : " . mysqli_connect_error();
+        // }
+        $servername = "185.27.134.10";
+        $username = "epiz_28500943";
+        $password = "2kiHEuD0ypoRX4";
+        $dbname = "epiz_28500943_proto_kms_bbppt";
+
+        // Create connection
+        $koneksi = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($koneksi->connect_error) {
+            die("Connection failed: " . $koneksi->connect_error);
+        } 
     ?>
