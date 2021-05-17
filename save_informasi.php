27d3aacc17350959447ec6c7ff5047d569f6baa7
iputuaw <awerdikatama@gmail.com>
2021-05-17 14:30:45 +0700

    debug insert dokumen

--- save_informasi.php
+++ save_informasi.php
@@ -24,7 +24,12 @@ if (!empty($judul)) {
   $pindah = move_uploaded_file($tmp,'src/image/'.$nama_baru);
   $query = $koneksi->query("INSERT INTO informasi (dokumen,judul,nomordokumen,keterangan,tanggal,restricted,idperangkat,iddivisi) VALUES('$nama_baru','$judul','$nomordokumen','$keterangan','$tanggal','$restricted','$idperangkat','$iddivisi')");
 
-  echo "<script>alert('Data berhasil ditambahkan'); window.location.href='informasi.php'</script>";
+  if ($query){
+    echo "<script>alert('Data berhasil ditambahkan'); window.location.href='informasi.php'</script>";
+
+  }else{
+    echo "Error: " . $query . "<br>" . $koneksi->error;
+  }
 }else{
   echo "<script>alert('Data gagal ditambahkan'); window.location.href='informasi.php'</script>";
 }
