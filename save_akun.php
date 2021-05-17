9740344ccaf7a3f0d6ded8972554096415adf07a
iputuaw <awerdikatama@gmail.com>
2021-05-17 13:04:35 +0700

    update insert akun

--- save_akun.php
+++ save_akun.php
@@ -10,15 +10,17 @@ if (isset($_POST['submit'])) {
   $level = $_POST['level'];
   $iddivisi = $_POST['iddivisi'];
 //divisi blm
-  // id_produk bernilai '' karena kita set auto increment
-  $q = $koneksi->query("INSERT INTO akun (username, password, nama, email, nip, iddivisi, level) VALUES ( '$username', MD5('$password'), '$nama', '$email', '$nip','$iddivisi', '$level')");
+  $query= "INSERT INTO akun (username, password, nama, email, nip, iddivisi, level) VALUES ('$username', MD5('$password'), '$nama', '$email', '$nip','$iddivisi', '$level')";
+  $result=mysqli_query($koneksi,$query);
+  //$q = $koneksi->query("INSERT INTO akun (username, password, nama, email, nip, iddivisi, level) VALUES ( '$username', MD5('$password'), '$nama', '$email', '$nip','$iddivisi', '$level')");
 
-  if ($q) {
+  if ($query) {
     // pesan jika data tersimpan
     echo "<script>alert('Data berhasil ditambahkan'); window.location.href='akun.php'</script>";
   } else {
     // pesan jika data gagal disimpan
     echo "<script>alert('Data gagal ditambahkan'); window.location.href='akun.php'</script>";
+    echo "Error: " . $sql . "<br>" . $conn->error;
   }
 } else {
   // jika coba akses langsung halaman ini akan diredirect ke halaman index
